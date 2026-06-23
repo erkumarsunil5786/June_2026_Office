f56ea73f3235315e8bda7091d3eff81643c79fe2
erkumarsunil5786 <erkumarsunil5786@gmail.com>
2026-06-24 02:13:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,5 +16,5 @@ resource "azurerm_virtual_network" "vnet2" {
   name                = "prod-vnet"
   resource_group_name = azurerm_resource_group.rg2.name
   location            = azurerm_resource_group.rg2.location
-  address_space       = ["10.0.1.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
