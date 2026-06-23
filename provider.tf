4952f78e1b586840ec56f079ba6300c2ffb41a35
erkumarsunil5786 <erkumarsunil5786@gmail.com>
2026-06-24 00:33:15 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+required_providers {
+azurerm = {
+source = "hashicorp/azurerm"
+version = "4.78.0"
+}
+}
+}
+provider "azurerm" {
+features {}
+subscription = "2b3920e9-30f7-44c4-9a49-07db5fc6bcae"
+}
